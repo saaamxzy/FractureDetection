7c0c9d11a66afe951ca16be78b4e0575623ad7c1
Zhengyuan <zhx024@ucsd.edu>
2017-07-16 23:47:47 -0700

    Fixed white borders.

--- extract_image.py
+++ extract_image.py
@@ -1,17 +1,32 @@
-import os
+import os, errno
 from shutil import copyfile
 
-PathDicom = './Images/'
+def init():
+	try:
+		os.makedirs('./images_to_read')
+		os.makedirs('./original_images')
+		os.makedirs('./test_images')
+		os.makedirs('./out_images')
+		os.makedirs('./final_images')
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
 
-lstFilesDCM = []  # create an empty list
-c = 0
-for dirName, subdirList, fileList in os.walk(PathDicom):
-    for filename in fileList:
-        if ".dcm" in filename.lower():  # check whether the file's DICOM
-            #lstFilesDCM.append(os.path.join(dirName,filename))
-            f = os.path.join(dirName,filename)
-            if os.path.getsize(f) > 10000:
-	            copyfile(os.path.join(dirName,filename), 
-	            	os.path.join('./images_to_read','test_' + str(c) + '.dcm'))
+
+if __name__ == '__main__':
+	init()
+	
+	PathDicom = './Images/'
+
+	lstFilesDCM = []  # create an empty list
+	c = 0
+	for dirName, subdirList, fileList in os.walk(PathDicom):
+	    for filename in fileList:
+	        if ".dcm" in filename.lower():  # check whether the file's DICOM
+	            #lstFilesDCM.append(os.path.join(dirName,filename))
+	            f = os.path.join(dirName,filename)
+	            if os.path.getsize(f) > 10000:
+		            copyfile(os.path.join(dirName,filename), 
+		            	os.path.join('./images_to_read','test_' + str(c) + '.dcm'))
 	            c+=1
 
--- save_image.py
+++ save_image.py
@@ -1,86 +1,107 @@
 from util import *
 
-for i in range(num_of_images + 1):
-	ds = dicom.read_file("./images_to_read/test_" + str(i) + ".dcm")
-	img = ds.pixel_array # retrieve the image array
+if __name__ == '__main__':
+	for i in range(18,21):
+		print 'Working on # ' + str(i)
+		ds = dicom.read_file("./images_to_read/test_" + str(i) + ".dcm")
+		img = ds.pixel_array # retrieve the image array
+		img = img[50:-50, 50:-50] # discard the black borders
+		# Uncomment to see original .dcm images
+		# save_image(img, 
+		#  	pylab.cm.bone, './original_images/original_' + str(i))
 
-	nrows, ncols = img.shape
-	sig = (nrows / 229.0, ncols / 187.0)
-	# set the value of sigma according to the image's scale
+		nrows, ncols = img.shape
+		sig = (nrows / 229.0, ncols / 187.0)
+		# set the value of sigma according to the image's scale
 
-	img = ndimage.gaussian_filter(img, sigma=sig, order=0, truncate=2.0)
+		img = ndimage.gaussian_filter(img, sigma=sig, order=0, truncate=2.0)
 
-	# plt.imshow(img, interpolation='nearest', cmap=pylab.cm.bone)
-	# plt.savefig('./gaussian', bbox_inches='tight')
+		# plt.imshow(img, interpolation='nearest', cmap=pylab.cm.bone)
+		# plt.savefig('./gaussian', bbox_inches='tight')
 
-	global_thresh = threshold_otsu(img)
-	th = global_thresh * 1.1
-	binary_global = img > th # temp threshold
+		global_thresh = threshold_otsu(img)
+		th = global_thresh
+		binary_global = img > th
 
-	# fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
-	# ax = axes.ravel()
-	plt.gray()
+		# fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
+		# ax = axes.ravel()
+		plt.gray()
 
-	mask = binary_global > 0.8 * binary_global.mean()
+		# To find the leftmost, rightmost, upmost and downmost black pixel
+		rows_z, cols_z = np.where(binary_global == False)
+		leftmost_z = cols_z.min()
+		rightmost_z = cols_z.max()
+		upmost_z = rows_z.min()
+		downmost_z = rows_z.max()
 
-	all_labels = measure.label(mask)
-	blobs_labels, n_labels = measure.label(mask, background=0, return_num=True)
+		# Debugging statements
+		# print binary_global
+		# print 'image dimension: ', binary_global.shape
+		# print leftmost_z, rightmost_z, upmost_z, downmost_z
 
-	sizes = ndimage.sum(mask, blobs_labels, range(n_labels + 1))
+		# To crop out the unwanted white borders
+		h,w = binary_global.shape
+		x_offset = w // 4
+		x_left_bound = w // 2 - x_offset
+		x_right_bound = w // 2 + x_offset
+		y_offset = h //4
+		y_up_bound = h // 2 - y_offset
+		y_down_bound = h // 2 + y_offset
 
-	# To find the leftmost, rightmost, upmost and downmost black pixel
-	rows_z, cols_z = np.where(blobs_labels == 0)
-	leftmost_z = cols_z.min()
-	rightmost_z = cols_z.max()
-	upmost_z = rows_z.min()
-	downmost_z = rows_z.max()
+		if not x_left_bound < leftmost_z < x_right_bound:
+			binary_global[0:h,0:leftmost_z+1] = False
+		if not x_left_bound < rightmost_z < x_right_bound:
+			binary_global[0:h, rightmost_z-1:w] = False
+		if not y_up_bound < upmost_z < y_down_bound:
+			binary_global[0:upmost_z+1, 0:w] = False
+		if not y_up_bound < downmost_z < y_down_bound:
+			binary_global[downmost_z-1:h, 0:w] = False
 
-	h,w = blobs_labels.shape
-	blobs_labels[0:leftmost_z+1, rightmost_z-1:w] = 0
-	blobs_labels[0:upmost_z+1, downmost_z-1:w] = 0
+		# Uncomment to see the binary image
+		# save_image(binary_global, None, './test_images/img' + str(i))
 
-	if sizes.size > 1:
-		temp = sizes.copy()
-		temp.sort()
-		minsize = temp[-2]
+		mask = binary_global > 0.8 * binary_global.mean()
 
-		mean_vals = ndimage.sum(img, blobs_labels, range(1, n_labels + 1))
+		all_labels = measure.label(mask)
+		blobs_labels, n_labels = measure.label(mask, background=0, return_num=True)
+		sizes = ndimage.sum(mask, blobs_labels, range(n_labels + 1))
 
-		mask_size = sizes < minsize + 1
-		remove_pixel = mask_size[blobs_labels]
-		blobs_labels[remove_pixel] = 0
+		if sizes.size > 1:
+			temp = sizes.copy()
+			temp.sort()
+			minsize = temp[-2]
+			mean_vals = ndimage.sum(img, blobs_labels, range(1, n_labels + 1))
+			mask_size = sizes < minsize + 1
+			remove_pixel = mask_size[blobs_labels]
+			blobs_labels[remove_pixel] = 0
 
+		# None-zeros
+		# To find all none zero fields
+		rows, cols = np.nonzero(blobs_labels)
+		leftmost = cols.min()
+		rightmost = cols.max()
+		upmost = rows.min()
+		downmost = rows.max()
 
-	# None-zeros
-	# To find all none zero fields in the image
-	rows, cols = np.nonzero(blobs_labels)
-	leftmost = cols.min()
-	rightmost = cols.max()
-	upmost = rows.min()
-	downmost = rows.max()
+		width = rightmost - leftmost
+		height = downmost - upmost
 
-	width = rightmost - leftmost
-	height = downmost - upmost
+		w_z = rightmost_z - leftmost_z
+		h_z = downmost_z - upmost_z
 
-	w_z = rightmost_z - leftmost_z
-	h_z = downmost_z - upmost_z
+		rect = make_rect(leftmost, upmost, width, height)
 
-	rect = make_rect(leftmost, upmost, width, height)
+		# Uncomment to see rectangles on grey scale image
+		# save_image(blobs_labels, 
+		#	'nipy_spectral', './out_images/img'+str(i)+'_cct', rect=rect)
 
-	black_rect = make_rect(leftmost_z, upmost_z, w_z, h_z)
+		save_image(ds.pixel_array, 
+			pylab.cm.bone, './final_images/img_final_' + str(i), rect=rect)
 
-        # save in connected components mode
-	save_image(blobs_labels, 
-		'nipy_spectral', './out_images/img'+str(i)+'_cct', rect=rect)
-        # save in final(original) mode
-	# save_image(ds.pixel_array, 
-	# 	pylab.cm.bone, './final_images/img_final_' + str(i), rect=black_rect)
+		#save_image(binary_global, None, './test_images/img' + str(i))
 
-        # save in binary mode
-	#save_image(binary_global, None, './test_images/img' + str(i))
-
-	# plt.imshow(binary_global)
-	# plt.axis('off')
-
-
+		# plt.imshow(binary_global)
+		# plt.axis('off')
+		#plt.savefig('./test_images/img')
+		#plt.show()
 
